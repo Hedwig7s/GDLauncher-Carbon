fa696a812c058af453c3b5a88123bca643d306fe
Hedwig7s <51961150+Hedwig7s@users.noreply.github.com>
2025-07-23 17:05:27 +0200

    Fuck you

--- apps/desktop/packages/mainWindow/src/global.d.ts
+++ apps/desktop/packages/mainWindow/src/global.d.ts
@@ -73,9 +73,9 @@ declare global {
 
 declare module "solid-js" {
   namespace JSX {
-    interface IntrinsicElements {
+    /*interface IntrinsicElements {
       owadview: any
-    }
+    }*/
   }
 }
 
